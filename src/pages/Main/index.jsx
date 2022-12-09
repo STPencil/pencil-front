7fd3fc45161d170d7220ed30c97bc2e36fbd9bca
yubin <dbqls2821@gmail.com>
2022-12-09 18:37:45 +0900

    home 페이지 대략 완성

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,17 +1,21 @@
 /* eslint-disable */
 
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import {Link} from "react-router-dom";
+import {Link, Navigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import FileItem from './fileItem';
 import axios from "axios";
-import setting from "../../assets/icon/setting.png"
+import logout from "../../assets/img/logout.png"
 import plusIcon from "../../assets/icon/plus_document.png"
-import folder from "../../assets/img/folder.png"
+import FileList from './fileList';
+import { render } from "@testing-library/react";
+
 
 const MainWrapper = styled.div`
 
   width: 100%;
-  height: 100%;
+  height: 100vh;
   background-color: #FAFAFF;  
   .top{
     height: 10%;
@@ -29,7 +33,7 @@ const MainWrapper = styled.div`
       align-items: center;
       display: flex;  
     }
-    .setting-icon{
+    .logout-icon{
       width: 10%;
       margin: 25px;
       width: 40px;
@@ -53,6 +57,7 @@ const MainWrapper = styled.div`
       margin: 10px;
       text-align: center;
       color: white;
+      border: none;
     }
     .align-button{
       font-weight: 500;
@@ -62,17 +67,17 @@ const MainWrapper = styled.div`
       border-radius: 30px;
       width: 170px;
       height: 50px;
-    margin: 10px;
+      margin: 10px;
       text-align: center;
+      border: none;
     }
   }
   .bottom{
     padding-top: 90px;
     padding-bottom: 120px;
-    padding-left: 200px;
+    padding-left: 100px;
     padding-right: 200px;
     display: flex;
-    flex-wrap: wrap;
     width: 90%;
     .plus-document{
       background-color: white;
@@ -90,28 +95,109 @@ const MainWrapper = styled.div`
       }
     }
 
-    .folder-group{
-      height: 210px;
-      width: 146px;
-      display: block;
-      padding-top: 25px;
-      margin-right: 100px;
-      .folder{
-        width: 160px;
-      }
-      .folder-name{
-        font-weight: 400;
-        font-size: 32px;
-        line-height: 44px;
-        text-align: center;
-        padding-left: 10px;
+
+    .files{
+      display: flex;
+      flex-wrap: wrap;
+      width: 100%;
+      .file-group{
+        display: block;
+        padding-top: 25px;
+        margin-right: 100px;
+        .file{
+          width: 160px;
+        }
+        .file-name{
+          font-weight: 400;
+          font-size: 20px;
+          line-height: 44px;
+          text-align: center;
+          padding-left: 10px;
+        }
       }
     }
   }
   
 `
 
-const Index = () => {
+export default function Index(){
+
+  const [files, setFiles] = useState([]);
+  const [origin, setOrigin] = useState([]);
+  const [buttonNum, setButtonNum] = useState(new Number(1));
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/user/homeDesc', {
+      headers: {
+        Authorization: "bearer " + localStorage.getItem('token')
+      }
+    })
+    .then(response => {
+      setFiles(response.data.result);
+      setOrigin(response.data.result);
+    });
+  }, []);
+
+  var clickedButton = new Number(1)
+  function onClickDesc(){
+    setButtonNum(new Number(1))
+    setFiles([...origin])
+  }
+
+  const ascSortResult = [...files].sort(function(a,b){
+    let x = a.id
+    let y = b.id
+    if(x<y){
+      return -1
+    } 
+    if(x>y){
+      return 1
+    }
+    return 0
+  })
+
+  const onClickAsc = async () => {
+    setButtonNum(new Number(2))
+    setFiles(ascSortResult)
+  }
+
+  const nameSortResult = [...files].sort(function(a,b){
+    let x = a.title.toLowerCase()
+    let y = b.title.toLowerCase()
+    if(x<y){
+      return -1
+    } 
+    if(x>y){
+      return 1
+    }
+    return 0
+  })
+
+  const onClickName = async () => {
+    setButtonNum(new Number(3))
+    setFiles(nameSortResult)
+  }
+
+
+  const navigate = useNavigate();
+  const onClickLogout = async () => {
+    var confirmLogout = confirm("로그아웃 하시겠습니까?")
+    if(confirmLogout){
+      localStorage.removeItem('token')
+      navigate('/login')
+    }
+  }
+
+
+  const fileInput = React.useRef(null);
+  const handleButtonClick = e => {
+    fileInput.current.click();
+  };
+  const handleChange = e => {
+    console.log(e.target.files[0]);
+  };
+
+  console.log('final log' + files)
 
   return(
     <MainWrapper>
@@ -119,78 +205,45 @@ const Index = () => {
         <div className="title">
           MyPaper
         </div>
-        <img className="setting-icon" src={setting} alt="setting" />
+        <img className="logout-icon" onClick={onClickLogout} src={logout} alt="logout" />
       </div>
       <div className="middle">
-        <div className="align-button-selected">
-          날짜
-        </div>
-        <div className="align-button">
-          이름
-        </div>
-        <div className="align-button">
-          유형
-        </div>
+        <button className={buttonNum == 1 ? "align-button-selected" : "align-button"} onClick={() => onClickDesc()}>
+          최신순
+        </button>
+        <button className={buttonNum == 2 ? "align-button-selected" : "align-button"} onClick={() => onClickAsc()}>
+          오래된순
+        </button>
+        <button className={buttonNum == 3 ? "align-button-selected" : "align-button"} onClick={() => onClickName()}>
+          이름순
+        </button>
       </div>
       <div className="bottom">
-        <div className="plus-document">
-          <img className="plus-icon" src={plusIcon} alt="plus document" />
+        <div onClick={handleButtonClick}>
+          <button className="plus-document">
+            <img className="plus-icon" src={plusIcon} alt="plus document"/>
+          </button>
         </div>
-        <div className="folder-group">
-          <img className="folder" src={folder} alt="folder" />
-          <div className="folder-name">mobile</div>
-        </div>
-        <div className="folder-group">
-          <img className="folder" src={folder} alt="folder" />
-          <div className="folder-name">mobile</div>
-        </div>
-        <div className="folder-group">
-          <img className="folder" src={folder} alt="folder" />
-          <div className="folder-name">mobile</div>
-        </div>
-        <div className="folder-group">
-          <img className="folder" src={folder} alt="folder" />
-          <div className="folder-name">mobile</div>
-        </div>
-        <div className="folder-group">
-          <img className="folder" src={folder} alt="folder" />
-          <div className="folder-name">mobile</div>
-        </div>
-        <div className="folder-group">
-          <img className="folder" src={folder} alt="folder" />
-          <div className="folder-name">mobile</div>
-        </div>
-        <div className="folder-group">
-          <img className="folder" src={folder} alt="folder" />
-          <div className="folder-name">mobile</div>
-        </div>
-        <div className="folder-group">
-          <img className="folder" src={folder} alt="folder" />
-          <div className="folder-name">mobile</div>
-        </div>
-        <div className="folder-group">
-          <img className="folder" src={folder} alt="folder" />
-          <div className="folder-name">mobile</div>
-        </div>
-        <div className="folder-group">
-          <img className="folder" src={folder} alt="folder" />
-          <div className="folder-name">mobile</div>
-        </div>
-        <div className="folder-group">
-          <img className="folder" src={folder} alt="folder" />
-          <div className="folder-name">mobile</div>
-        </div>
-        <div className="folder-group">
-          <img className="folder" src={folder} alt="folder" />
-          <div className="folder-name">mobile</div>
-        </div>
+        <input style={{display: "none"}} type="file" id="fileUpload" ref={fileInput} onChange={handleChange}/>
         
-        
+        {/* <div className="folder-group">
+          <img className="folder" src={folder} alt="folder" />
+          <div className="folder-name">mobile</div>
+        </div> */}
+        {/* <FileList props={files} /> */}
 
+        <div className="files">
+            {files && files.map((file, index) => (
+              <div className="file-group" key={index}>
+                <img className='file' src={file.preview_img} style={{borderRadius: "10px"}}></img>
+                <div className='file-name'>{file.title}</div>
+              </div>
+              // <FileItem  key={index} id={file.id} userId={file.user_id} createdAt={file.created_at} title={file.title} pdfUrl={file.pdf_url} previewImg={file.preview_img} />
+            ))}
+        </div>
+        
       </div>
 
     </MainWrapper>
   );
 };
-
-export default Index;
